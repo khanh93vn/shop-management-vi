5d92d50954ea9e93b5a00515a85fdba9e3e12bd8
khanh93vn <khanh93vn@gmail.com>
2022-02-03 17:13:46 +0700

    implement search(), fix database setup bug in tests

--- src/backend/users/manager.py
+++ src/backend/users/manager.py
@@ -1,3 +1,4 @@
+from sqlalchemy import String
 from .sql import User
 
 class UsersManager:
@@ -6,14 +7,32 @@ class UsersManager:
     def __init__(self, session):
         self.session = session
     
-    def search(self, info):
-        pass
+    def search(self, **info):
+        query = self.session.query(self.data_class)
+        for key, value in info.items():
+            col = getattr(self.data_class, key)
+            if isinstance(col.type, String):
+                query = query.filter(col.like('%'+value+'%'))
+            else:
+                query = query.filter(col == value)
+        return query.all()
     
     def get_row(self, column, value):
         pass
     
     def add_new(self, data):
         pass
+        # if isinstance(data, dict)
+            # entry = self.data_class(**data)
+        # elif isinstance(data, (tuple, list)):
+            # entry = self.data_class(
+                # **{key:value 
+                #    for key, value in
+                #    zip(self.data_class.__table__.columns.keys(), data)})
+        # else:
+            # raise TypeError("Must be a dict, tuple or list")
+        # self.session.add(entry)
+        # self.session.commit()
     
     def delete(self, id):
         pass
--- tests/backend/users/test_manager.py
+++ tests/backend/users/test_manager.py
@@ -5,16 +5,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from src.backend.users.manager import UsersManager
+from src.backend.base.sql import Base
 from src.backend.users.sql import User
 
 DB = "testdatabase.db"
 engine = create_engine("sqlite:///{}".format(DB), echo=True)
 Session = sessionmaker(bind=engine)
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True, scope='function')
 def setup_and_cleanup_database():
     if os.path.exists(DB):
         os.remove(DB)
+    Base.metadata.create_all(engine)
     yield
     
     if os.path.exists(DB):
@@ -22,32 +24,30 @@ def setup_and_cleanup_database():
 
 class TestUsersManager:
     def test_fields(self):
-        session = Session()
-        man = UsersManager(session)
+        with Session() as session:
+            man = UsersManager(session)
         assert man.session is session
-        session.close()
     
     def test_search_empty(self):
-        session = Session()
-        man = UsersManager(session)
+        with Session() as session:
+            man = UsersManager(session)
         assert man.search() == []
-        session.close()
     
     def test_add_new_tuple(self):
-        session = Session()
-        man = UsersManager(session)
-        new_usr = ("foo", "bar")
-        man.add_new(new_usr)
-        added_usr, = man.search()
+        with Session() as session:
+            Base.metadata.create_all(engine)
+            man = UsersManager(session)
+            new_usr = ("foo", "bar")
+            man.add_new(new_usr)
+            added_usr, = man.search()
         assert (added_usr.name, added_usr.password) == new_usr
-        session.close()
     
     def test_add_new_dict(self):
-        session = Session()
-        man = UsersManager(session)
-        new_usr = dict(name="foo", password="bar")
-        man.add_new(new_usr)
-        added_usr, = man.search()
+        with Session() as session:
+            Base.metadata.create_all(engine)
+            man = UsersManager(session)
+            new_usr = dict(name="foo", password="bar")
+            man.add_new(new_usr)
+            added_usr, = man.search()
         assert added_usr.name == new_usr['name']
         assert added_usr.password == new_usr['password']
-        session.close()
